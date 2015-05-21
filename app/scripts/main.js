8427e2d88ae9b8ca32fd3e6666548ab362ab4488
nerea <nereamoreno23@gmail.com>
2015-05-21 21:30:44 +0200

    1 subida

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,46 @@
-console.log('\'Allo \'Allo!');
+'use strict'; 
+$(document).ready(function () { 
+	$("input").prop('required',true);//valida todos los campos del formulario a la vez
+    
+    $('#form1').validate({
+    	rules: {
+            nombre: {
+                minlength: 4,
+            },
+            email: {
+                email:true,
+                remote: 'http://localhost/formularioAjax/validar_email_db.php'
+            },
+            email2: {
+                equalTo: '#email'
+            },
+            
+        },
+
+        messages: {
+            nombre: {
+                required: 'Debes escribir tu nombre',
+                minlength: 'Debes introducir al menos 4 carácteres'
+            },
+            email: {
+                email:'El correo no es válido',
+                required: 'Debes escribir tu correo',
+                remote: 'El email ya existe'
+            },
+            email2: {
+                equalTo:'El correo debe ser igual al anterior'
+            }
+        }
+    }); 
+
+    $('#form2').validate({
+
+
+    });
+
+
+    $('#form3').validate({
+
+
+    });
+});
